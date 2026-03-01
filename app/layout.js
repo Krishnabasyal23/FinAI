f1a7a870e9aaf91c18d5e0845aefbf4147cdf685
Apathbyte <bidhanthapa2022@gmail.com>
2026-03-01 00:52:05 -0500

    Initial commit

---
+++ app/layout.js
@@ -0,0 +1,28 @@
+import { Inter} from "next/font/google";
+import "./globals.css";
+import Header from "@/components/header"
+
+const inter = Inter({subsets: ["latin"]});
+export const metadata = {
+  title: "FinAI",
+  description: "Your solution to finance tracking",
+};
+
+export default function RootLayout({ children }) {
+  return (
+    <html lang="en">
+      <body
+        className={`${inter.className}`}
+      >{/*Header*/}
+      <Header></Header>
+      <main className="min-h-screen">{children}</main>
+        {/*footer*/}
+        <footer className="bg-grey-50 py-12">
+          <div className="container mx-auto px-4 text-center text-black-600">
+            <p>Made by Bidhan and the Crew</p>
+          </div>
+        </footer>
+      </body>
+    </html>
+  );
+}
